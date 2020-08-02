e552e5a216420fc02e664efa67a9c96390396a54
Bilal <Bilal@DESKTOP101>
2020-08-02 22:58:07 +1000

    Added Solution/Project Structure

---
+++ src/SevenTest.ConsoleOutput/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SevenTest.ConsoleOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seven West Media Technical Assingment Console Output");
+            Console.WriteLine("====================================================");
+
+            //TODO Add Logic To Display Required Output instead of comming Soon
+            Console.WriteLine("Real Output comming soon");
+        }
+    }
+}
